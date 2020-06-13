8e00a8ec4d497b94b7d3464bafd880535bfe9001
rizkiiryanto96 <a2.1800155@mhs.stmik-sumedang.ac.id>
2020-06-13 12:51:34 +0700

    Menyelesaikan UAS PABWEB

---
+++ Pendaftaran_UKM/form_ubah.php
@@ -0,0 +1,127 @@
+<?php 
+
+if (isset($_GET['nim'])) { 
+    
+    $nim    = $_GET['nim']; 
+    $query  = mysqli_query($db, "SELECT * FROM tbl_pendaftaran WHERE nim='$nim'") or die('Query Error : '.mysqli_error($db)); 
+    $data   = mysqli_fetch_assoc($query); 
+
+    $nim            = $data['nim']; 
+    $nama_lengkap   = $data['nama_lengkap'];
+    $jenis_kelamin  = $data['jenis_kelamin'];
+    $program_studi  = $data['program_studi'];
+    $alamat         = $data['alamat'];
+    $pilih_ukm      = $data['pilih_ukm'];
+    $no_hp          = $data['no_hp'];  
+ } 
+
+ mysqli_close($db); 
+ ?>
+
+<div class="row"> 
+    <div class="col-md-12">
+        <div class="alert alert-info" role="alert"> 
+            <i class="fas fa-edit"></i> Ubah Data 
+        </div> 
+
+        <div class="card"> 
+            <div class="card-body"> 
+                <form class="needs-validation" action="proses_ubah.php" method="post" enctype="multipart/form-data" novalidate> 
+                    <div class="row"> 
+                        <div class="col"> 
+
+                         
+
+                            <div class="form-group col-md-12"> 
+                                <label>NIM</label> 
+                                <input type="text" class="form-control" name="nim" maxlength="10" autocomplete="off" value="<?php echo $nim; ?>" readonly required> 
+                                <div class="invalid-feedback">NIM tidak boleh kosong.</div> 
+                            </div> 
+
+                            <div class="form-group col-md-12"> 
+                                <label>Nama Lengkap</label> 
+                                <input type="text" class="form-control" name="nama_lengkap" autocomplete="off" value="<?php echo $nama_lengkap; ?>" required> 
+                                <div class="invalid-feedback">Nama Lengkap tidak boleh kosong.</div> 
+                            </div>
+
+                            <div class="form-group col-md-12"> 
+                                <label class="mb-3">Jenis Kelamin</label> 
+                            <?php 
+                            if ($jenis_kelamin=='Laki-laki') { ?> 
+                                <div class="custom-control custom-radio"> 
+                                    <input type="radio" class="custom-control-input" id="customControlValidation2" name="jenis_kelamin" value="Laki-laki" checked required> 
+                                    <label class="custom-control-label" for="customControlValidation2">Laki-laki</label> 
+                                </div> 
+                                <div class="custom-control custom-radio mb-4"> 
+                                    <input type="radio" class="custom-control-input" id="customControlValidation3" name="jenis_kelamin" value="Perempuan" required> 
+                                    <label class="custom-control-label" for="customControlValidation3">Perempuan</label> 
+                                    <div class="invalid-feedback">Pilih salah satu jenis kelamin.</div> 
+                                </div> 
+                            <?php 
+                            } else { ?> 
+                            <div class="custom-control custom-radio">
+                                <input type="radio" class="custom-control-input" id="customControlValidation2" name="jenis_kelamin" value="Laki-laki" required> 
+                                <label class="custom-control-label" for="customControlValidation2">Laki-laki</label> 
+                            </div> 
+                            <div class="custom-control custom-radio mb-4">
+                                <input type="radio" class="custom-control-input" id="customControlValidation3" name="jenis_kelamin" value="Perempuan" checked required>
+                                <label class="custom-control-label" for="customControlValidation3">Perempuan</label> 
+                                <div class="invalid-feedback">Pilih salah satu jenis kelamin.</div>
+                            </div> 
+                        <?php } ?> 
+                        </div>
+
+                        <div class="form-group col-md-12"> 
+                            <label>Program Studi</label> 
+                            <select class="form-control" name="program_studi" autocomplete="off" required> 
+                                <option value="<?php echo $program_studi; ?>"><?php echo $program_studi; ?></option>
+                                <option value=""></option>
+                                <option value="Teknik Informatika">Teknik Informatika</option> 
+                                <option value="Sistem Informasi">Sistem Informasi</option> 
+                            </select> 
+                            <div class="invalid-feedback">Program Studi tidak boleh kosong.</div> 
+                        </div> 
+                    </div>
+
+                    <div class="col"> 
+                        <div class="form-group col-md-12"> 
+                            <label>Alamat</label> 
+                            <textarea class="form-control" rows="2" name="alamat" autocomplete="off" required> <?php echo $alamat; ?></textarea> 
+                            <div class="invalid-feedback">Alamat tidak boleh kosong.</div> 
+                        </div>
+
+                        <div class="form-group col-md-12"> 
+                            <label>No. HP</label> 
+                            <input type="text" class="form-control" name="no_hp" maxlength="13" onKeyPress="return goodchars(event,'0123456789',this)" autocomplete="off" value="<?php echo $no_hp; ?>" required>
+                            <div class="invalid-feedback">No. HP tidak boleh kosong.</div> 
+                        </div> 
+
+                        <div class="form-group col-md-12">
+                                <label>Pilih UKM</label>
+                                <select class="form-control" name="pilih_ukm" autocomplete="off" required>
+                                    <option value="<?php echo $pilih_ukm; ?>"><?php echo $pilih_ukm; ?></option>
+                                    <option value=""></option> 
+                                    <option value="DJ">DJ</option> 
+                                    <option value="Kajis">Kajis</option>
+                                    <option value="Paduan Suara">Paduan Suara</option> 
+                                    <option value="Photography">Photograpy</option>
+                                    <option value="Radio">Radio</option>
+                                    <option value="SIM C">SIM C</option>
+                                </select> 
+                                <div class="invalid-feedback">Pilih UKM tidak boleh kosong.</div> 
+                        </div>
+                    </div> 
+                    
+                </div>
+
+                <div class="my-md-4 pt-md-1 border-top"> </div> 
+
+                <div class="form-group col-md-12 right"> 
+                    <input type="submit" class="btn btn-info btn-submit mr-2" name="simpan" value="Simpan"> 
+                    <a href="index.php" class="btn btn-secondary btn-reset"> Batal </a> 
+                </div> 
+            </form> 
+        </div>
+    </div> 
+</div> 
+</div> 
